7c0fd0ff101c7544c6e22dddb69dde84eb5b92a0
stogey25 <79273019+stogey25@users.noreply.github.com>
2021-02-18 11:45:37 -0500

    Delete messages.py

--- basic_auth/messages.py
+++
@@ -1,5 +0,0 @@
-def get_secret_message():
-    return ("The secret messages are calling to me endlessly, " +
-    "they call to me across the air, " +
-    "the messages across the atmosphere, " +
-    "they whisper in your ear, they're calling everywhere")
